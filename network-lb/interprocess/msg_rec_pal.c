a147d0abed350a36b5dda52531a79c622cf31db5
Tharikh <tharikhlegend@gmail.com>
2021-05-08 17:02:31 +0530

    message passing complete

---
+++ network-lb/interprocess/msg_rec_pal.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<string.h>
+
+struct buf{
+    long type;
+    char msg[100];
+};
+
+int isPallindrome(char *str){
+    int last=strlen(str)-1,first=0;
+    while(first<last){
+        if(str[first++] != str[last--]){
+            return 0;
+        }
+    }
+    return 1;
+}
+void main(){
+    struct buf message;
+    key_t key;
+    int msgid;
+
+    key=ftok("msg_send_pal.c",95);
+
+    msgid=msgget(key,0666 | IPC_CREAT);
+
+    msgrcv(msgid,&message,sizeof(message),1,0);
+
+    printf("Data recieved :%s\n",message.msg);
+    if(isPallindrome(message.msg)){
+        printf("It is Pallindrome\n");
+    }else{
+        printf("It is not Pallindrome\n");
+    }
+    msgctl(msgid, IPC_RMID, NULL);
+}
---
+++ network-lb/interprocess/msg_send_pal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+struct buf{
+    long type;
+    char msg[100];
+};
+
+void main(){
+    struct buf message;
+    key_t key;
+    int msgid;
+
+    key=ftok("msg_send_pal.c",95);
+
+    msgid=msgget(key,0666 | IPC_CREAT);
+    message.type=1;
+
+    printf("enter string : ");
+    scanf("%s",message.msg);
+
+    msgsnd(msgid,&message,sizeof(message),0);
+
+    printf("Data send \n");
+}
