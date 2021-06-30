43189ee2519069f767cc290757020deabf59ba3f
Tharikh <tharikhlegend@gmail.com>
2021-06-30 10:48:00 +0530

    readers writers problem

--- network-lb/second-readers-writers/reader.c
+++ network-lb/second-readers-writers/reader.c
@@ -10,23 +10,24 @@
 int readcount=0;
 #define MAX 10
 struct semdetails{
-    sem_t *writer,*mutex;
+    sem_t *writer,*mutex,*reader;
     pid_t id;
 };
 void *readerfun(void * args){
     struct semdetails *details=(struct semdetails *)args;
-    sem_t *writer=details->writer,*mutex=details->mutex;
+    sem_t *writer=details->writer,*mutex=details->mutex,*reader=details->reader;
+
+    sem_wait(reader);
     sem_wait(mutex);
     readcount++;
     if(readcount == 1){
         sem_wait(writer);
     }
     sem_post(mutex);
+    sem_post(reader);
 
     key_t key = ftok("reader.c",5);
     int shmid = shmget(key,1024,0666|IPC_CREAT);
-
-    
     int *num = (int*) shmat(shmid,(void*)0,0);
     printf("Data read by %d is : %d\n",details->id,*num);
     shmdt(num);
@@ -41,19 +42,22 @@ void *readerfun(void * args){
 
 }
 void main(){
-    sem_t *writer,*mutex;
-    const char *writer_name="writer";
-    const char *mutex_name="mutex";
+    sem_t *writer,*mutex,*reader;
+    const char *writer_name="writer2";
+    const char *mutex_name="mutex1";
+    const char *reader_name="reader2";
     pthread_t readers[MAX];
     struct semdetails detail[MAX];
 
     writer = sem_open(writer_name,O_CREAT,0666,1);
     mutex = sem_open(mutex_name,O_CREAT,0666,1);
+    reader = sem_open(reader_name,O_CREAT,0666,1);
 
     for (int i = 0; i < MAX; i++)
     {
         detail[i].writer=writer;
         detail[i].mutex=mutex;
+        detail[i].reader=reader;
         detail[i].id=i;
         pthread_create(&readers[i],NULL,readerfun,(void *)&detail[i]);
     }
@@ -62,9 +66,8 @@ void main(){
         pthread_join(readers[i],NULL);
     }
     
-    sem_close(writer);sem_close(mutex);
-    sem_unlink(writer_name);sem_unlink(mutex_name);
-
+    sem_close(writer);sem_close(mutex);sem_close(reader);
+    sem_unlink(writer_name);sem_unlink(mutex_name);sem_unlink(reader_name);
     key_t key = ftok("reader.c",5);
     int shmid = shmget(key,1024,0666|IPC_CREAT);
     shmctl(shmid,IPC_RMID,NULL);
--- network-lb/second-readers-writers/writer.c
+++ network-lb/second-readers-writers/writer.c
@@ -7,15 +7,22 @@
 #include <fcntl.h>
 #include<unistd.h>
 
+int writecount=0;
 #define MAX 10
 struct semdetails{
-    sem_t *writer,*mutex;
+    sem_t *writer,*mutex,*reader;
     pid_t id;
 };
-void *writerfun(void * args){
+void *readerfun(void * args){
     struct semdetails *details=(struct semdetails *)args;
-    sem_t *writer=details->writer,*mutex=details->mutex;
-    
+    sem_t *writer=details->writer,*mutex=details->mutex,*reader=details->reader;
+
+    sem_wait(mutex);
+    writecount++;
+    if(writecount == 1){
+        sem_wait(reader);
+    }
+    sem_post(mutex);
     sem_wait(writer);
 
     key_t key = ftok("reader.c",5);
@@ -26,36 +33,41 @@ void *writerfun(void * args){
     shmdt(num);
 
     sem_post(writer);
+    sem_wait(mutex);
+    writecount--;
+    if(writecount == 0){
+        sem_post(reader);
+    }
+    sem_post(mutex);
 
 }
 void main(){
-    sem_t *writer,*mutex;
-    const char *writer_name="writer";
-    const char *mutex_name="mutex";
+    sem_t *writer,*mutex,*reader;
+    const char *writer_name="writer2";
+    const char *mutex_name="mutex2";
+    const char *reader_name="reader2";
     pthread_t writers[MAX];
     struct semdetails detail[MAX];
 
     writer = sem_open(writer_name,O_CREAT,0666,1);
     mutex = sem_open(mutex_name,O_CREAT,0666,1);
+    reader = sem_open(reader_name,O_CREAT,0666,1);
 
     for (int i = 0; i < MAX; i++)
     {
         detail[i].writer=writer;
         detail[i].mutex=mutex;
+        detail[i].reader=reader;
         detail[i].id=i;
-        pthread_create(&writers[i],NULL,writerfun,(void *)&detail[i]);
+        pthread_create(&writers[i],NULL,readerfun,(void *)&detail[i]);
     }
     for (int i = 0; i < MAX; i++)
     {
         pthread_join(writers[i],NULL);
     }
     
-    sem_close(writer);sem_close(mutex);
-    sem_unlink(writer_name);sem_unlink(mutex_name);
-    
-    key_t key = ftok("reader.c",5);
-    int shmid = shmget(key,1024,0666|IPC_CREAT);
-    shmctl(shmid,IPC_RMID,NULL);
+    sem_close(writer);sem_close(mutex);sem_close(reader);
+    sem_unlink(writer_name);sem_unlink(mutex_name);sem_unlink(reader_name);
 
 
 }
