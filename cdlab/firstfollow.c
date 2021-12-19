96f9959eba45b20aa9714f1d55d191cff474bb9d
Tharikh <tharikhlegend@gmail.com>
2021-12-20 00:56:04 +0530

    follow start

--- cdlab/firstfollow.c
+++ cdlab/firstfollow.c
@@ -1,14 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
 #define CHARLENGTH 20
-/*
-S->aA
-S->Ab
-A->Ba
-B->b
-*/
+
 int isTerminal(char *terminals,char terminal,int n){
     for (int i = 0; i < n; i++)
     {
@@ -97,7 +91,7 @@ void firstOfNonTerminal(char *nonterminals,int index,char **production,char *ter
                         addToFirst('e',first,index);
                         continue;
                     }
-                    printf("%c ",temp[0]);
+                    // printf("%c ",temp[0]);
                     tempindex=findIndexOfNonterminal(nonterminals,temp[0],m);
                 if(first[tempindex][0] != '\0'){
                     copyFirst(index,tempindex,first);
@@ -123,6 +117,44 @@ void firstOfNonTerminal(char *nonterminals,int index,char **production,char *ter
         
     }
 }
+void followOfNonTerminal(char *nonterminals,int index,char **production,char *terminals,char ** first,int m,int n,int num,int check){
+    char *temp;
+    int tempindex;
+    if(follow[index][0] != '\0' && index != 0){
+        return;
+    }else{
+        for (int i = 0; i < num; i++)
+        {
+        int j=0;
+        temp=production[i]+3;
+        while(temp[j] != '\0'){
+            if(nonterminals[index] == temp[j]){
+                L2:
+                if(isTerminal(terminals,temp[j+1]),n){
+                    addToFirst(temp[j+1],follow,index);
+                }else{
+                    if(temp[j+1] == '\0'){
+                        tempindex=findIndexOfNonterminal(nonterminals,producion[i][0],m);
+                        followOfNonTerminal(nonterminals,tempindex,production,terminals,first,m,n,num,index);
+                        copyFirst(i,index,follow);
+                    }else{
+                        tempindex=findIndexOfNonterminal(nonterminals,temp[j+1],m);
+                        copyFollow(tempindex,index,follow,first);
+                        if(haveEpsilon(tempindex,first)){
+                            j++;
+                            goto L2;
+                        }
+                    }
+                }
+            }
+            j++;
+        }
+        }
+    }
+    
+    
+
+}
 void findFirsts(char *nonterminals,char *terminals,char **production,char **first,int m,int n,int num){
     for (int i = 0; i < m; i++)
     {
@@ -130,9 +162,18 @@ void findFirsts(char *nonterminals,char *terminals,char **production,char **firs
     }
     
 }
+void findFollow(char *nonterminals,char *terminals,char **production,char **first,char **follow,int m,int n,int num){
+    
+    follow[0][0]='$';follow[0][1]='\0';
+    for (int i = 0; i < m; i++)
+    {
+        followOfNonTerminal(nonterminals,i,production,terminals,first,m,n,num,-1);
+    }
+    
+}
 void main(){
     int m=6,n=6,num=9;
-    char *terminals=malloc(sizeof(char) * n),*nonterminals=malloc(sizeof(char)*m),**production=malloc(sizeof(char *) * num),**first=malloc(sizeof(char *) * m);
+    char *terminals=malloc(sizeof(char) * n),*nonterminals=malloc(sizeof(char)*m),**production=malloc(sizeof(char *) * num),**first=malloc(sizeof(char *) * m),**follow=malloc(sizeof(char *) * m);
     for (int i = 0; i < num; i++)
     {
         production[i]=malloc(sizeof(char) * CHARLENGTH);
@@ -141,6 +182,8 @@ void main(){
     {
         first[i]=malloc(sizeof(char) *CHARLENGTH);
         first[i][0]='\0';
+        follow[i]=malloc(sizeof(char) * CHARLENGTH);
+        follow[i][0]='\0';
     }
     
     terminals[0]='a';terminals[1]='h';terminals[2]='c';terminals[3]='b';terminals[4]='g';terminals[5]='f';
@@ -150,7 +193,9 @@ void main(){
     strcpy(production[5],"E->g");strcpy(production[6],"E->e");strcpy(production[7],"F->f");strcpy(production[8],"F->e");
 
     findFirsts(nonterminals,terminals,production,first,m,n,num);
+    findFollow(nonterminals,terminals,production,first,follow,m,n,num);
     int j=0;
+    printf("FIRST \n");
     for (int i = 0; i < m; i++)
     {
         printf("%c = {",nonterminals[i]);
@@ -163,5 +208,17 @@ void main(){
         
         printf("}\n");
     }
-    
+    printf("FOLLOW \n");
+    for (int i = 0; i < m; i++)
+    {
+        printf("%c = {",nonterminals[i]);
+        j=0;
+        while (follow[i][j] != '\0')
+        {
+            printf("%c,",follow[i][j]);
+            j++;
+        }
+        
+        printf("}\n");
+    }
 }
